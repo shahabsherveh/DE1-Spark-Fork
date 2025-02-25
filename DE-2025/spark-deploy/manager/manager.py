0122a7e33160c603418cb6d292f1a516ac5b6318
Usama Zafar <usama-zafar@outlook.com>
2025-02-25 14:30:03 +0100

    Bug removal in path.

--- DE-2025/spark-deploy/manager/manager.py
+++ DE-2025/spark-deploy/manager/manager.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route("/drain-node", methods=["POST"])
 def drain_node():
     if request.method == "POST":
-        subprocess.call(f"bash /DE1-Spark/DE-2025/spark-deploy/manager/drain.sh", shell=True)
+        subprocess.call(f"bash /home/ubuntu/DE1-Spark/DE-2025/spark-deploy/manager/drain.sh", shell=True)
         # return Response(json.dumps({"nodes": node_names}), 200)
         return Response("Successfully drained the ndoe!", 200)
     else:
