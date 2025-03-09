312771dfd39c9610ec9d7ab81b772ce58d95f8f4
Shahab <shahabsherveh@gmail.com>
2025-03-09 08:04:48 +0100

    deleted:    DE-2025/spark-deploy/delete_cluster.sh

--- DE-2025/spark-deploy/delete_cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-prefix=group-3-spark
-floating_ip="$(cat floating_ip.txt)"
-
-instances="$(openstack  server list | grep $prefix-* | awk '{print $2}')"
-openstack server delete $instances
